6c06e977c6557c2c7e69b36dd38e88b5079a86cf
Arundas <51848726+arundas005@users.noreply.github.com>
2024-08-14 14:38:02 +0530

    Code refactoring

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,15 +1,29 @@
-#include <assert.h>
 #include <iostream>
 #include "misaligned.h"
+#include "TestPrintColorMap.h"
 
-int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+int colorPairNumber( int majorColorIndex, int minorColorIndex) {
+    return majorColorIndex * 5 + minorColorIndex;
+}
+
+std::string formatOneLine(int colorPairIndex, std::string majorColor, std::string minorColor) {
+    return std::to_string(colorPairIndex) + " | " + majorColor + " | " + minorColor;
+}
+
+void printColorMap() {
     int i = 0, j = 0;
     for (i = 0; i < 5; i++) {
         for (j = 0; j < 5; j++) {
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
+            std::cout << formatOneLine(colorPairNumber(i, j) , majorColor[i], minorColor[j]) << std::endl;
         }
     }
-    return i * j;
+}
+
+int main() {
+    printColorMap();
+    testPrintColorMap();
+    return 0;
 }
