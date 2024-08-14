02931ada738b5ac6ee30b6b6636e12eda30cef09
Arundas <51848726+arundas005@users.noreply.github.com>
2024-08-14 14:39:46 +0530

    Code refactoring

--- TestPrintColorMap.cpp
+++ TestPrintColorMap.cpp
@@ -1,43 +1,149 @@
 #include "misaligned.h"
-#include "tshirts.h"
-#include <gtest\gtest.h>
-#include <string>
-#include <iostream>
+#include "TestPrintColorMap.h"
 #include <sstream>
+#include <iostream>
+#include <vector>
+#include <assert.h>
 
-std::string removeLineEndings(const std::string& input) {
-    std::string result = input;
-    result.erase(std::remove_if(result.begin(), result.end(), [](char c) {
-        bool isMatch = false;
-        if(!std::isalnum(static_cast<unsigned char>(c)))
+std::vector<std::vector<std::string> > expectedContent = {
+        {"1", "White", "Blue"},
+        {"2", "White", "Orange"},
+        {"3", "White", "Green"},
+        {"4", "White", "Brown"},
+        {"5", "White", "Slate"},
+        {"6", "Red", "Blue"},
+        {"7", "Red", "Orange"},
+        {"8", "Red", "Green"},
+        {"9", "Red", "Brown"},
+        {"10", "Red", "Slate"},
+        {"11", "Black", "Blue"},
+        {"12", "Black", "Orange"},
+        {"13", "Black", "Green"},
+        {"14", "Black", "Brown"},
+        {"15", "Black", "Slate"},
+        {"16", "Yellow", "Blue"},
+        {"17", "Yellow", "Orange"},
+        {"18", "Yellow", "Green"},
+        {"19", "Yellow", "Brown"},
+        {"20", "Yellow", "Slate"},
+        {"21", "Violet", "Blue"},
+        {"22", "Violet", "Orange"},
+        {"23", "Violet", "Green"},
+        {"24", "Violet", "Brown"},
+        {"25", "Violet", "Slate"}
+    };
+
+std::vector<std::string> tokenizeString(const std::string& input) {
+    std::vector<std::string> tokens;
+    std::istringstream iss(input);
+    std::string line;
+    while (std::getline(iss, line)) {
+        tokens.push_back(line);
+    }
+    return tokens;
+}
+
+bool isDelimiterPresent(const std::string& input, char delimiter, int position1, int position2) {
+    if (position1 < 0 || position1 >= input.length() || position2 < 0 || position2 >= input.length()) {
+        return false;
+    }
+    
+    if (input[position1] == delimiter && input[position2] == delimiter) {
+        return true;
+    }
+    
+    return false;
+}
+
+bool IsFormatCorrect(const std::vector<std::string>& tokens)
+{
+    bool isFormatCorrect = true;
+    for (const std::string& token : tokens)
+    {
+        if (token.length() < 2)
         {
-            if( c != '|' && c != ' ')
-            {
-                isMatch = true;
-            }
+            isFormatCorrect = false;
+            break;
         }
-        return isMatch;
-    }), result.end());
+        if (!isDelimiterPresent(token, '|', 6, 19))
+        {
+            isFormatCorrect = false;
+            break;
+        }
+    } 
+    return isFormatCorrect;
+}
+
+std::string removeWhiteSpaces(const std::string& input) {
+    std::string result;
+    for (char c : input) {
+        if (c != ' ') {
+            result += c;
+        }
+    }
     return result;
 }
 
+std::vector<std::string> tokenizeString(const std::string& input, char delimiter) {
+    std::vector<std::string> tokens;
+    std::istringstream iss(input);
+    std::string token;
+    while (std::getline(iss, token, delimiter)) {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
 
-TEST(PrintColorMap, testPrintColorMap)
+bool checkColorMapContentOfEachLine(const std::vector<std::string>& tokens, const std::vector<std::string>& expectedTokens)
+{
+    bool isContentCorrect = true;
+    std::cout<<"Actual: " <<tokens[0] << " " << tokens[1] << " " << tokens[2] << std::endl;
+    std::cout << "Expected: " << expectedTokens[0] << " " << expectedTokens[1] << " " << expectedTokens[2] << std::endl;
+    for (int i = 0; i < expectedTokens.size(); i++)
+    {
+        if (tokens[i] != expectedTokens[i])
+        {
+            isContentCorrect = false;
+            break;
+        }
+    }
+    return isContentCorrect;
+}
+
+bool checkColorMapContent(const std::vector<std::string>& actualColorMapPrinted, const std::vector<std::vector<std::string> >& expectedTokens)
+{
+    bool isContentCorrect = true;
+    for (int i = 0; i < expectedTokens.size(); i++)
+    {
+        std::string line = removeWhiteSpaces(actualColorMapPrinted[i]);
+        std::vector<std::string> tokenizedCodeAndColors = tokenizeString(line, '|');
+        if (!checkColorMapContentOfEachLine(tokenizedCodeAndColors, expectedTokens[i]))
+        {
+            isContentCorrect = false;
+            break;
+        }
+    }   
+    return isContentCorrect;
+}
+
+void testPrintColorMap()
 {
     //redirect console output to a stringstream
     std::ostringstream output;
     std::streambuf* oldCoutStreamBuf = std::cout.rdbuf();
     std::cout.rdbuf(output.rdbuf());
-    int actualResult = printColorMap();
+    printColorMap();
     std::cout.rdbuf(oldCoutStreamBuf); // Restore the original cout buffer
     std::string capturedOutput = output.str();
-    std::string actual = removeLineEndings(capturedOutput);
-    std::string expected = "1 | White | Blue2 | White | Blue3 | White | Blue4 | \
-White | Blue5 | White | Blue6 | Red | Orange7 | Red | Orange8 | Red | \
-Orange9 | Red | Orange10 | Red | Orange11 | Black | Green12 | Black | \
-Green13 | Black | Green14 | Black | Green15 | Black | Green16 | Yellow | \
-Brown17 | Yellow | Brown18 | Yellow | Brown19 | Yellow | Brown20 | Yellow | \
-Brown21 | Violet | Slate22 | Violet | Slate23 | Violet | Slate24 | \
-Violet | Slate25 | Violet | Slate";
-    EXPECT_EQ(actual, expected);
+
+    //get each line of the output
+    std::vector<std::string>& actualContent = tokenizeString(capturedOutput);
+    
+    //check printed color map format
+    bool isFormatCorrect = IsFormatCorrect(actualContent);
+    assert(isFormatCorrect);
+
+    //check printed color map content
+    bool IsContentCorrect = checkColorMapContent(actualContent, expectedContent);
+    assert(IsContentCorrect);
 }
