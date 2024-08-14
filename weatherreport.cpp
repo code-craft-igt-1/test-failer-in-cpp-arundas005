12f4feecae27d04d0e4f8985a5809bfddb108f3c
Arundas <51848726+arundas005@users.noreply.github.com>
2024-08-14 14:28:19 +0530

    Code refactoring

--- weatherreport.cpp
+++ weatherreport.cpp
@@ -1,44 +1,34 @@
-#include <assert.h>
 #include <string>
 #include <iostream>
 #include "weatherreport.h"
+#include "TestWeatherReport.h"
 
 using std::cout, std::endl, std::string;
 
 namespace WeatherSpace {
 
 
-        int SensorStub::Humidity() const {
-            return 72;
-        }
+// This is a function to predict the weather, based on readings
+// from a sensor
 
-        int SensorStub::Precipitation() const {
-            return 70;
-        }
+string Report(const IWeatherSensor& sensor) {
+    int precipitation = sensor.Precipitation();
+    // precipitation < 20 is a sunny day
+    string report = "Sunny day";
 
-        double SensorStub::TemperatureInC() const {
-            return 26;
-        }
-
-        int SensorStub::WindSpeedKMPH() const {
-            return 52;
-        }
-
-    // This is a function to predict the weather, based on readings
-    // from a sensor
-
-    string Report(const IWeatherSensor& sensor) {
-        int precipitation = sensor.Precipitation();
-        // precipitation < 20 is a sunny day
-        string report = "Sunny day";
-
-        if (sensor.TemperatureInC() > 25) {
-            if (precipitation >= 20 && precipitation < 60)
-                report = "Partly cloudy";
-            else if (sensor.WindSpeedKMPH() > 50)
-                report = "Alert, Stormy with heavy rain";
-        }
-        return report;
+    if (sensor.TemperatureInC() > 25) {
+        if (precipitation >= 20 && precipitation < 60)
+            report = "Partly cloudy";
+        else if (sensor.WindSpeedKMPH() > 50)
+            report = "Alert, Stormy with heavy rain";
     }
+    return report;
+}
 
 }  // namespace WeatherSpace
+
+int main() {
+    WeatherSpace::TestRainy();
+    WeatherSpace::TestHighPrecipitationAndLowWindspeed();
+    return 0;
+}
