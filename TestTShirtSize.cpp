49d21a057b0aadc70683cc7e2d5734dbfe50ab65
Arundas <51848726+arundas005@users.noreply.github.com>
2024-08-14 14:35:54 +0530

    Code refactoring

--- TestTShirtSize.cpp
+++ TestTShirtSize.cpp
@@ -1,51 +1,10 @@
-#include "tshirts.h"
-#include <gtest\gtest.h>
+#include"tshirts.h"
+#include <assert.h>
 
-TEST(TShirtSize, testSmallsize)
-{
-    char actualSize = size(37);
-    char expectedSize = 'S';
-    EXPECT_EQ(actualSize, expectedSize);
-}
-
-TEST(TShirtSize, testSmallsizeUpperLimit)
-{
-    char actualSize = size(38);
-    char expectedSize = 'S';
-    EXPECT_EQ(actualSize, expectedSize);
-}
-
-TEST(TShirtSize, testMediumsize)
-{
-    char actualSize = size(40);
-    char expectedSize = 'M';
-    EXPECT_EQ(actualSize, expectedSize);
-}
-
-TEST(TShirtSize, testMediumLowerLimit)
-{
-    char actualSize = size(39);
-    char expectedSize = 'M';
-    EXPECT_EQ(actualSize, expectedSize);
-}
-
-TEST(TShirtSize, testMediumUpperLimit)
-{
-    char actualSize = size(42);
-    char expectedSize = 'M';
-    EXPECT_EQ(actualSize, expectedSize);
-}
-
-TEST(TShirtSize, testLargesize)
-{
-    char actualSize = size(45);
-    char expectedSize = 'L';
-    EXPECT_EQ(actualSize, expectedSize);
-}
-
-TEST(TShirtSize, testLargesizeLowerLimit)
-{
-    char actualSize = size(43);
-    char expectedSize = 'L';
-    EXPECT_EQ(actualSize, expectedSize);
-}
+void testTshirtSizeFromSholderWidthInCMS() {
+    assert(tshirtSizeFromSholderWidthInCMS(37) == 'S');
+    assert(tshirtSizeFromSholderWidthInCMS(38) == 'M');
+    assert(tshirtSizeFromSholderWidthInCMS(42) == 'L');
+    assert(tshirtSizeFromSholderWidthInCMS(43) == 'L');
+    assert(tshirtSizeFromSholderWidthInCMS(100) == 'L');
+}  
