868eeeb0331e1895c6d3dc2513fb60ae62471be7
Arundas <51848726+arundas005@users.noreply.github.com>
2024-08-23 10:17:16 +0530

    high precipitation forecast handled

--- weatherreport.cpp
+++ weatherreport.cpp
@@ -19,6 +19,8 @@ string Report(const IWeatherSensor& sensor) {
     if (sensor.TemperatureInC() > 25) {
         if (precipitation >= 20 && precipitation < 60)
             report = "Partly cloudy";
+        else if(precipitation >= 60)
+            report = "Alert, Heavy rain";
         else if (sensor.WindSpeedKMPH() > 50)
             report = "Alert, Stormy with heavy rain";
     }
